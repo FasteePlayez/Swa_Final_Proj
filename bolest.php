9985ea647c6b6f218beaf9907fc132303ca6d04a
Pavel Bartoš <129551427+FasteePlayez@users.noreply.github.com>
2024-05-30 20:02:09 +0200

    bolestivy skript

--- bolest.php
+++ bolest.php
@@ -9,11 +9,16 @@
         <div class="bolest">
             <a href="index.php">Zpět</a>
         </div>
-      
 
-        
+        <div>
+            <script src=".js/bolestiveskriptovani.js"></script>
+            <button onclick="bolestos()">Bolest?</button>
+        </div>
+
+
+
     </body>
 
 
-    
+
 </html>
